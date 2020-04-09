d16f8bd89fc2f3dd57a652071d35caafcfad9dc0
Aitor López <aitorlopez98@opendeusto.es>
2020-04-09 12:59:18 +0200

    app admin

--- envE2/projectoE2/appProduccion/admin.py
+++ envE2/projectoE2/appProduccion/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Equipo, Empleado, Proceso
 
 # Register your models here.
+admin.site.register(Equipo)
+admin.site.register(Empleado)
+admin.site.register(Proceso)
--- envE2/projectoE2/appProduccion/models.py
+++ envE2/projectoE2/appProduccion/models.py
@@ -2,19 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Proceso(models.Model):
-    cof = models.CharField(max_length=75) #Mezcla numeros y letras
-    cp = models.CharField(primary_key=true, max_length=75) #Mezcla numeros y letras
-    nom_pro = models.CharField(max_length=100)
-    ref = models.CharField(max_length=75) #Mezcla numeros y letras
-    f_inicio = models.DateField()
-    f_fin = models.DateField()
-    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
-    empleados = models.ManyToMany(Empleado)
-
-    def __str__(self):
-        return f"ID: {self.id}, Codigo OF: {self.cof}, C. Preceso: {self.cp}, Nombre: {self.nom_pro}, Referencia: {self.ref}, F. Inicio: {self.f_inicio}, F. Fin: {self.f_fin}"
-
 class Equipo(models.Model):
     modelo = models.CharField(max_length=100)
     marca = models.CharField(max_length=100)
@@ -26,7 +13,7 @@ class Equipo(models.Model):
         return f"ID: {self.id}, Modelo: {self.modelo}, Marca: {self.marca}, Categoria: {self.categoria}, F. Adquisicion: {self.f_adquisicion}, F. Instalacion: {self.f_intalacion}"
 
 class Empleado(models.Model):
-    dni = models.CharField(primary_key=true, max_length=20) #Para introducir DNIs extranjeros
+    dni = models.CharField(primary_key=True, max_length=20) #Para introducir DNIs extranjeros
     nombre = models.CharField(max_length=100) #Por nombres compuestos
     apellidos = models.CharField(max_length=150) #Dos apellidos
     email = models.CharField(max_length=100)
@@ -36,3 +23,16 @@ class Empleado(models.Model):
 
     def __str__(self):
         return f"ID: {self.id}, DNI: {self.dni}, Nombre: {self.nombre}, Apellidos: {self.apellidos}, Email: {self.email}, Telefono: {self.tel}, Direccion: {self.dir}, F. Nacimiento: {self.f_nacimiento}"
+
+class Proceso(models.Model):
+    cof = models.CharField(max_length=75) #Mezcla numeros y letras
+    cp = models.CharField(primary_key=True, max_length=75) #Mezcla numeros y letras
+    nom_pro = models.CharField(max_length=100)
+    ref = models.CharField(max_length=75) #Mezcla numeros y letras
+    f_inicio = models.DateField()
+    f_fin = models.DateField()
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+    empleados = models.ManyToManyField(Empleado)
+
+    def __str__(self):
+        return f"ID: {self.id}, Codigo OF: {self.cof}, C. Preceso: {self.cp}, Nombre: {self.nom_pro}, Referencia: {self.ref}, F. Inicio: {self.f_inicio}, F. Fin: {self.f_fin}"
