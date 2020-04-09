0b40abef81869bc79a92e6f9e82cbf9a7f5db8cd
Aitor López <aitorlopez98@opendeusto.es>
2020-04-09 13:33:15 +0200

    comentarios

---
+++ envE2/projectoE2/appProduccion/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Equipo, Empleado, Proceso
+
+"""Formulario Equipo"""
+class EquipoForm(forms.ModelForm):
+    class Meta:
+        model = Equipo
+        fields = '__all__'
+
+"""Formulario Empleado"""
+class EmpleadoForm(forms.ModelForm):
+    class Meta:
+        model = Empleado
+        fields = '__all__'
+
+"""Formulario Proceso"""
+class ProcesoForm(forms.ModelForm):
+    class Meta:
+        model = Proceso
+        fields = '__all__'
--- envE2/projectoE2/appProduccion/models.py
+++ envE2/projectoE2/appProduccion/models.py
@@ -13,11 +13,15 @@ class Equipo(models.Model):
         return f"ID: {self.id}, Modelo: {self.modelo}, Marca: {self.marca}, Categoria: {self.categoria}, F. Adquisicion: {self.f_adquisicion}, F. Instalacion: {self.f_intalacion}"
 
 class Empleado(models.Model):
-    dni = models.CharField(primary_key=True, max_length=20) #Para introducir DNIs extranjeros
-    nombre = models.CharField(max_length=100) #Por nombres compuestos
-    apellidos = models.CharField(max_length=150) #Dos apellidos
+    """Para introducir DNIs extranjeros"""
+    dni = models.CharField(primary_key=True, max_length=20)
+    """Por nombres compuestos"""
+    nombre = models.CharField(max_length=100)
+    """Dos apellidos"""
+    apellidos = models.CharField(max_length=150)
     email = models.CharField(max_length=100)
-    tel = models.IntegerField(default=666666666) #Valor defecto ejemplo
+    """Valor defecto ejemplo"""
+    tel = models.IntegerField(default=666666666)
     dir = models.CharField(max_length=150)
     f_nacimiento = models.DateField()
 
@@ -25,10 +29,13 @@ class Empleado(models.Model):
         return f"ID: {self.id}, DNI: {self.dni}, Nombre: {self.nombre}, Apellidos: {self.apellidos}, Email: {self.email}, Telefono: {self.tel}, Direccion: {self.dir}, F. Nacimiento: {self.f_nacimiento}"
 
 class Proceso(models.Model):
-    cof = models.CharField(max_length=75) #Mezcla numeros y letras
-    cp = models.CharField(primary_key=True, max_length=75) #Mezcla numeros y letras
+    """ cof numeros y letras"""
+    cof = models.CharField(max_length=75)
+    """ cp Mezcla numeros y letras"""
+    cp = models.CharField(primary_key=True, max_length=75)
     nom_pro = models.CharField(max_length=100)
-    ref = models.CharField(max_length=75) #Mezcla numeros y letras
+    """ ref Mezcla numeros y letras"""
+    ref = models.CharField(max_length=75)
     f_inicio = models.DateField()
     f_fin = models.DateField()
     equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
