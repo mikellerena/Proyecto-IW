cfc4751995f9235143f080657949751b097e68b0
Aitor López <aitorlopez98@opendeusto.es>
2020-04-11 12:36:41 +0200

    mas urls

--- envE2/projectoE2/appProduccion/urls.py
+++ envE2/projectoE2/appProduccion/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('createequipo/', views.CreateEquipoView.as_view(), name='equipo_form'),
     path('equipos/', views.EquiposListView.as_view(), name='equipos'),
-    path('equipo/<int:pk>/', views.EquiposDetailView.as_view(), name='equipo'),
+    path('equipo/<int:pk>/', views.EquiposDetailView.as_view(), name='equipo_detail'),
     path('createempleado/', views.CreateEmpleadoView.as_view(), name='empleado_form'),
-    path('')
+    path('empleados/', views.EmpleadosListView.as_view(), name='empleados'),
+    path('empleado/<int:pk>/', views.EmpleadosDetailView.as_view(), name='empleado_detail'),
 ]
