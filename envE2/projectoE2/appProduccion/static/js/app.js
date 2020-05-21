8a59077ae128ccf7793e96c58c90dabff43a6b9e
Aitor López <aitorlopez98@opendeusto.es>
2020-05-21 21:07:42 +0200

    fatcheando un poco

--- envE2/projectoE2/appProduccion/static/js/app.js
+++ envE2/projectoE2/appProduccion/static/js/app.js
@@ -23,3 +23,52 @@
 
         });
     });
+
+const URL = "http://127.0.0.1:8000/appProduccion/produccion/equipos/api/";
+
+let ver = document.getElementById('ver');
+ver.addEventListener('click', event => {
+    loadData();
+})
+
+function loadData(){
+    console.log(url);
+    fetch(URL)
+        .then((response) => response.json())
+        .then((json) => {
+            introducirTabla(json);
+        });
+}
+
+function crearTabla(json){
+    let tabla = `
+        <table>
+            <thead>
+                <tr>
+                    <td>ID</td>
+                    <td>Marca</td>
+                    <td>Modelo</td>
+                </tr>
+            </thead>
+            <tbody>`;
+
+    for (dato of json){
+        tabla += crearFila(dato.id, dato.marca, dato.modelo);
+    }
+    tabla += `</tbody></table>`;
+    return tabla;
+}
+
+function crearFila(id, marca, modelo){
+    return `
+        <tr>
+            <td>${id}</td>
+            <td>${marca}</td>
+            <td>${modelo}</td>
+        </tr>`;
+}
+
+function introducirTabla(json){
+    let tabla = crearTabla(json);
+    document.getElementById('cont').innerHTML = tabla;
+}
