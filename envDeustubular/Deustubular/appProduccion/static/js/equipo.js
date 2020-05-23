97ec8808ac87343c2144d6b6709e30b612640a8c
Aitor López <aitorlopez98@opendeusto.es>
2020-05-23 14:05:35 +0200

    comentado do no funciona

--- envDeustubular/Deustubular/appProduccion/static/js/equipo.js
+++ envDeustubular/Deustubular/appProduccion/static/js/equipo.js
@@ -1,63 +1,63 @@
 const URL = "http://127.0.0.1:8000/appProduccion/produccion/equipos/api/";
 
-let ver = document.getElementById('ver');
-ver.addEventListener('click', event => {
-    event.preventDefault();
-    loadData();
-})
+//let ver = document.getElementById('ver');
+//ver.addEventListener('click', event => {
+//    event.preventDefault();
+//    loadData();
+//})
+//
+//function loadData(){
+//    console.log(URL);
+//    fetch(URL)
+//        .then((response) => response.json())
+//        .then((json) => {
+//            introducirTabla(json);
+//        });
+//}
+//
+//function crearTabla(json){
+//    let tabla = `
+//        <table id="table-js">
+//            <thead>
+//                <tr>
+//                    <td>ID</td>
+//                    <td class="td_estilos">Marca</td>
+//                    <td class="td_estilos">Modelo</td>
+//                    <td class="td_estilos">Enlaces</td>
+//                </tr>
+//            </thead>
+//            <tbody>`;
+//
+//    for (let dato of json){
+//        tabla += crearFila(dato.id, dato.marca, dato.modelo);
+//    }
+//    tabla += `</tbody></table>`;
+//    return tabla;
+//}
+//
+//function crearFila(id, marca, modelo){
+//    return `
+//        <tr>
+//            <td>${id}</td>
+//            <td class="td_estilos">${marca}</td>
+//            <td class="td_estilos">${modelo}</td>
+//            <td class="td_estilos">
+//                <div class="mar-inferior">
+//                    <a href="http://127.0.0.1:8000/appProduccion/produccion/equipos/${id}/">Ver</a> ||
+//                    <a href="http://127.0.0.1:8000/appProduccion/produccion/equipos/${id}/update/">Editar</a> ||
+//                    <a href="http://127.0.0.1:8000/appProduccion/produccion/equipos/${id}/delete/">Borrar</a>
+//                </div>
+//            </td>
+//        </tr>`;
+//}
+//
+//function introducirTabla(json){
+//    let tabla = crearTabla(json);
+//    document.getElementById('cont').innerHTML = tabla;
+//}
 
-function loadData(){
-    console.log(URL);
-    fetch(URL)
-        .then((response) => response.json())
-        .then((json) => {
-            introducirTabla(json);
-        });
-}
 
-function crearTabla(json){
-    let tabla = `
-        <table id="table-js">
-            <thead>
-                <tr>
-                    <td>ID</td>
-                    <td class="td_estilos">Marca</td>
-                    <td class="td_estilos">Modelo</td>
-                    <td class="td_estilos">Enlaces</td>
-                </tr>
-            </thead>
-            <tbody>`;
-
-    for (let dato of json){
-        tabla += crearFila(dato.id, dato.marca, dato.modelo);
-    }
-    tabla += `</tbody></table>`;
-    return tabla;
-}
-
-function crearFila(id, marca, modelo){
-    return `
-        <tr>
-            <td>${id}</td>
-            <td class="td_estilos">${marca}</td>
-            <td class="td_estilos">${modelo}</td>
-            <td class="td_estilos">
-                <div class="mar-inferior">
-                    <a href="http://127.0.0.1:8000/appProduccion/produccion/equipos/${id}/">Ver</a> ||
-                    <a href="http://127.0.0.1:8000/appProduccion/produccion/equipos/${id}/update/">Editar</a> ||
-                    <a href="http://127.0.0.1:8000/appProduccion/produccion/equipos/${id}/delete/">Borrar</a>
-                </div>
-            </td>
-        </tr>`;
-}
-
-function introducirTabla(json){
-    let tabla = crearTabla(json);
-    document.getElementById('cont').innerHTML = tabla;
-}
-
-
-/*Busqueda por nombre de empleado*/
+/*Busqueda por marca de equipo*/
 
 
 let busq = document.getElementById('busq');
--- envDeustubular/Deustubular/appProduccion/static/js/proceso.js
+++ envDeustubular/Deustubular/appProduccion/static/js/proceso.js
@@ -1,69 +1,67 @@
 const URL = "http://127.0.0.1:8000/appProduccion/produccion/procesos/api/";
 
-let ver = document.getElementById('ver');
-ver.addEventListener('click', event => {
-    event.preventDefault();
-    loadData();
-})
+//let ver = document.getElementById('ver');
+//ver.addEventListener('click', event => {
+//    event.preventDefault();
+//    loadData();
+//})
+//
+//function loadData(){
+//    console.log(URL);
+//    fetch(URL)
+//        .then((response) => response.json())
+//        .then((json) => {
+//            introducirTabla(json);
+//        });
+//}
+//
+//function crearTabla(json){
+//    let tabla = `
+//        <div class="div-table">
+//            <table id="table-js">
+//                <thead>
+//                    <tr>
+//                        <td>ID</td>
+//                        <td class="td_estilos">Codigo Orden<br>Frabricación</td>
+//                        <td class="td_estilos">Codigo Proceso</td>
+//                        <td class="td_estilos">Referencia</td>
+//                        <td class="td_estilos">Nombre</td>
+//                        <td class="td_estilos">Enlaces</td>
+//                    </tr>
+//                </thead>
+//                <tbody>`;
+//
+//    for (let dato of json){
+//        tabla += crearFila(dato.id, dato.codigo_orden_fabricacion, dato.codigo_proceso, dato.referencia, dato.nombre_proceso);
+//    }
+//    tabla += `</tbody></table></div>`;
+//    return tabla;
+//}
+//
+//function crearFila(id, codigo_orden_fabricacion, codigo_proceso, referencia, nombre_proceso){
+//    return `
+//        <tr>
+//            <td>${id}</td>
+//            <td class="td_estilos">${codigo_orden_fabricacion}</td>
+//            <td class="td_estilos">${codigo_proceso}</td>
+//            <td class="td_estilos">${referencia}</td>
+//            <td class="td_estilos">${nombre_proceso}</td>
+//            <td class="td_estilos">
+//                <div class="mar-inferior">
+//                    <a href="http://127.0.0.1:8000/appProduccion/produccion/procesos/${id}/">Ver</a> ||
+//                    <a href="http://127.0.0.1:8000/appProduccion/produccion/procesos/${id}/update/">Editar</a> ||
+//                    <a href="http://127.0.0.1:8000/appProduccion/produccion/procesos/${id}/delete/">Borrar</a>
+//                </div>
+//            </td>
+//        </tr>`;
+//}
+//
+//function introducirTabla(json){
+//    let tabla = crearTabla(json);
+//    document.getElementById('cont').innerHTML = tabla;
+//}
 
-function loadData(){
-    console.log(URL);
-    fetch(URL)
-        .then((response) => response.json())
-        .then((json) => {
-            introducirTabla(json);
-        });
-}
-
-function crearTabla(json){
-    let tabla = `
-        <div class="div-table">
-            <table id="table-js">
-                <thead>
-                    <tr>
-                        <td>ID</td>
-                        <td class="td_estilos">Codigo Orden<br>Frabricación</td>
-                        <td class="td_estilos">Codigo Proceso</td>
-                        <td class="td_estilos">Referencia</td>
-                        <td class="td_estilos">Nombre</td>
-                        <td class="td_estilos">Enlaces</td>
-                    </tr>
-                </thead>
-                <tbody>`;
-
-    for (let dato of json){
-        tabla += crearFila(dato.id, dato.codigo_orden_fabricacion, dato.codigo_proceso, dato.referencia, dato.nombre_proceso);
-    }
-    tabla += `</tbody></table></div>`;
-    return tabla;
-}
-
-function crearFila(id, codigo_orden_fabricacion, codigo_proceso, referencia, nombre_proceso){
-    return `
-        <tr>
-            <td>${id}</td>
-            <td class="td_estilos">${codigo_orden_fabricacion}</td>
-            <td class="td_estilos">${codigo_proceso}</td>
-            <td class="td_estilos">${referencia}</td>
-            <td class="td_estilos">${nombre_proceso}</td>
-            <td class="td_estilos">
-                <div class="mar-inferior">
-                    <a href="http://127.0.0.1:8000/appProduccion/produccion/procesos/${id}/">Ver</a> ||
-                    <a href="http://127.0.0.1:8000/appProduccion/produccion/procesos/${id}/update/">Editar</a> ||
-                    <a href="http://127.0.0.1:8000/appProduccion/produccion/procesos/${id}/delete/">Borrar</a>
-                </div>
-            </td>
-        </tr>`;
-}
-
-function introducirTabla(json){
-    let tabla = crearTabla(json);
-    document.getElementById('cont').innerHTML = tabla;
-}
-
-
-/*Busqueda por nombre de empleado*/
-
+/*Busqueda por nombre de proceso*/
 
 let busq = document.getElementById('busq');
 busq.addEventListener('click', event => {
@@ -119,3 +117,4 @@ function introducirLista(json){
     let lista = crearLista(json);
     document.getElementById('cont').innerHTML = lista;
 }
+
